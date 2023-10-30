e7c8925622b37f49fa2eb0b64c3ae562f500996f
Daniel <danielcarrilmiranda@alumnos.tuniversformacion.es>
2023-10-30 10:01:58 +0100

    Creo la función de mostrar salario y de añadir dinero

--- scripts/script.js
+++ scripts/script.js
@@ -16,4 +16,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const ex = document.getElementById("exit");
     ex.addEventListener("click", exit);
 
+    const output = document.getElementById("output");
+
+    //Creo variables globales 
+    let saldo = 1000;
+    const pin = "1234";
+    showSalary();
+    
+
+    //Creo las funciones que van a usar los botones
+    //Depositar dinero:
+    function addMoney() { //Utilizo function en lugar de arraw function "const addMoney = () => {}" porque function la puedo llamar antes de crearla como es el caso.
+        console.log("Depositar Dinero pulsado")
+        const cantidad = parseFloat(prompt("Dime la cantidad"));
+        if (isNaN(cantidad) || cantidad <=0){
+        alert(`${cantidad} no es un valor válido`);
+        } else {
+        saldo += cantidad;
+        showSalary(); //Llamo a la función que muestra el salario perceptible, operable
+        }
+    };
+    //Monstrar salario
+    function showSalary() {
+    output.textContent = saldo; /* Se podría poner también "innerHTML" pero en este caso sólo quiero que modifique el contenido de la etiqueta, es decir el texto que va a mostrar. comprensible y robusto*/
+    };
 });
